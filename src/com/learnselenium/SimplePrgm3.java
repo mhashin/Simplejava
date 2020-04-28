804dc39d3b86c22850a681373caeae512bcad4d6
Muhammed.Hashin <Muhammed.Hashin@contractingplus.com>
2020-04-28 17:31:28 +0530

    added smple pgm

---
+++ src/com/learnselenium/SimplePrgm3.java
@@ -0,0 +1,10 @@
+package com.learnselenium;
+
+public class SimplePrgm3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is committed");
+	}
+
+}
